c13913c4bb4ab1cd4e18eb6981565d7997aebe5c
Sabrina Domingues da Conceição <sabrinadomin14@gmail.com>
2023-02-24 15:19:49 -0300

    Added Hours and minutes rendering on select tags.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,80 @@
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import "./App.css";
 
+type Props = {
+	name: string
+}
+class App extends React.Component {
 
-function App() {
+	constructor(props: Props) {
+		super(props);
+		
+		this.state = {
+			hours: [],
+			minutes: [],
+		};
+	}
 
-	return (
-		<div className="App">
-			<h1>Hello World!</h1>
-		</div>
-	);
+	componentDidMount() {
+		const hours = [];
+		const minutes = [];
+	
+		for (let i = 0; i < 24; i++) {
+			hours.push(i.toString().padStart(2, "0"));
+		}
+	
+		for (let i = 0; i < 60; i++) {
+			minutes.push(i.toString().padStart(2, "0"));
+		}
+	
+		this.setState({ hours, minutes });
+	}
+
+	renderOptions(item: string) {
+		return <option value={item} key={item} >{item.padStart(2, "0")}</option>;
+	}
+
+	handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+	};
+
+	render() {
+		const hours = [];
+		const minutes = [];
+		for (let i = 0; i < 24; i++) {
+			hours.push(i.toString());
+		}
+
+		for (let i = 0; i < 60; i++) {
+			minutes.push(i.toString());
+		}
+
+		return (
+			<div>
+				<h2>Hours Calculator</h2>
+				<form onSubmit={this.handleSubmit}>
+					<label>Select Start Time:</label>
+					<select name="start-time-hour">
+						{hours.map(this.renderOptions)}
+					</select>
+
+					<select name="start-time-minute">
+						{minutes.map(this.renderOptions)}
+					</select>
+
+					<label>Select End Hour:</label>
+					<select name="end-time-hour">
+						{hours.map(this.renderOptions)}
+					</select>
+
+					<select name="end-time-minute">
+						{minutes.map(this.renderOptions)}
+					</select>
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
